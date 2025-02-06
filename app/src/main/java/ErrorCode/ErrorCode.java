722c973d445418f45565f2c8ddf0d5c9265fbad3
bognaradam26 <bognaradam26@gmail.com>
2025-02-06 15:09:38 +0100

    classes

---
+++ app/src/main/java/ErrorCode/ErrorCode.java
@@ -0,0 +1,9 @@
+package ErrorCode;
+
+public class ErrorCode {
+    int Message;
+    String Remedy;
+    String Note1;
+    String Note2;
+
+}
---
+++ app/src/main/java/ErrorCode/ErrorCodeAdapter.java
@@ -0,0 +1,8 @@
+package ErrorCode;
+
+import java.util.List;
+
+public class ErrorCodeAdapter {
+
+    List<ErrorCode> errorCodes;
+}
